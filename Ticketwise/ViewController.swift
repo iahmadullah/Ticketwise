10f600a3f35e54052426434122850df300586578
aqdasmalik <aqdasmalikcs@gmail.com>
2020-05-03 00:14:56 +0500

    Initial Commit

---
+++ Ticketwise/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Ticketwise
+//
+//  Created by Ahmad Ullah on 03/05/2020.
+//  Copyright © 2020 Lolita Moratova. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
